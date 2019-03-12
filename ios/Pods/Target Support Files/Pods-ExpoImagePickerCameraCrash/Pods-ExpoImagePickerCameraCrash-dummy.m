9a2e39142754208699b7252200fca43af7ac8a25
Joel Shapiro <joel@getaction.tv>
2019-03-12 14:15:05 -0600

    pod install

---
+++ ios/Pods/Target Support Files/Pods-ExpoImagePickerCameraCrash/Pods-ExpoImagePickerCameraCrash-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExpoImagePickerCameraCrash : NSObject
+@end
+@implementation PodsDummy_Pods_ExpoImagePickerCameraCrash
+@end
---
+++ ios/Pods/Target Support Files/Pods-ExpoImagePickerCameraCrashTests/Pods-ExpoImagePickerCameraCrashTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ExpoImagePickerCameraCrashTests : NSObject
+@end
+@implementation PodsDummy_Pods_ExpoImagePickerCameraCrashTests
+@end
