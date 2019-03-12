3370f0103f405a8c770420404ab7c8e56c1d3c22
Joel Shapiro <joel@getaction.tv>
2019-03-12 14:50:30 -0600

    Setup App.js to check permissions and launch camera upon componentDidMount.

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, Alert } from 'react-native';
+import * as Permissions from 'expo-permissions';
+import * as ImagePicker from 'expo-image-picker';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -7,6 +9,32 @@ const instructions = Platform.select({
 });
 
 export default class App extends Component {
+  launchCamera = async () => {
+    console.log("checking permissions")
+    
+    try {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        throw new Error("You MUST grant permissions for this demo app to work!")
+      } else {
+        console.log("Permissions granted!")
+      }
+
+      await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [1, 1],
+        quality: 0.9,
+        base64: true,
+      }).then(() => Alert.alert("Yayyy, we took a photo!"))
+    } catch (e) {
+      Alert.alert(e.message);
+    }
+  }
+
+  async componentDidMount() {
+    await this.launchCamera();
+  }
+
   render() {
     return (
       <View style={styles.container}>
